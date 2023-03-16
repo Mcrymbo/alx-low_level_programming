92fb92f9b232fb6b9278e8d6b3805cc2c1dd8ee4
Mcrymbo <croco2023@gmail.com>
2023-03-16 10:45:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a musltilingual puzzle");
+	return (0);
+}
