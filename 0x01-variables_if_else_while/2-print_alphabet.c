6c6b495718248508838bb08342d8804784ebff26
Mcrymbo <croco2023@gmail.com>
2023-03-17 06:47:10 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - calculating alphabets
+ *
+ * Return: always 0 for sucess
+ */
+int main(void)
+{
+	char c[26] = 'abcdefghijklmnopqrstuvwxyz';
+	int b, i;
+
+	b = 26;
+	for (i = 0, i < 0, i++)
+	{
+		putchar(c[i]);
+	}
+	putchar("\n");
+	return (0);
+}
