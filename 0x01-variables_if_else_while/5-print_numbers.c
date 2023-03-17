d93f1590399d639d331d61e0aed90225837f0c97
Mcrymbo <croco2023@gmail.com>
2023-03-17 07:33:10 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers all single digits of base 10
+ *
+ * Return: always 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i)
+}
