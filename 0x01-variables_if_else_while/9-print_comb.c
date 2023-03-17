f1adf33f462883d230d5bc0e0fa9a1edb9b0f043
Mcrymbo <croco2023@gmail.com>
2023-03-17 08:41:03 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - printing combinations of single-digit letters
+ *
+ * Return: always 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
