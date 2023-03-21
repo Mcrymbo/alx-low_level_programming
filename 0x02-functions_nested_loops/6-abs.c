8a75b4144b70fa2650f6345c63bd647a93f3261d
Mcrymbo <croco2023@gmail.com>
2023-03-21 09:58:12 -0700

    checking for sign of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of a number
+ *
+ * @int: accepts an integer
+ * Return: positive number for success
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * (-1));
+	}
+}
