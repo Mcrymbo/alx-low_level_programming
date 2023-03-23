6083aee53b9ee7e694ea8cec0494726aaa9c8eb0
Mcrymbo <croco2023@gmail.com>
2023-03-23 19:45:06 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,34 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_triangle - prints a triangle.
- * @size: numbers of lines.
- * Return: no return.
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
  */
-void print_triangle(int size)
+int main(void)
 {
-	int i, j;
-	for (i = 0; i < size; i++)
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		for (j = 1; j < (size - i); j++)
-			_putchar(' ');
-		for (j--; j < size; j++)
-			_putchar(35);
-		if (i < (size - 1))
-			_putchar('\n');
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
 	}
-	_putchar('\n');
+	printf("\n");
+	return (0);
 }
