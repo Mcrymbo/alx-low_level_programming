90d2cf8b966a53a14baa1d2b038b77a41a265902
Mcrymbo <croco2023@gmail.com>
2023-03-23 18:38:40 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,10 @@ void print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i != 50 || i != 52)
 		{
 			_putchar(i);
 		}
 	}
 	_putchar('\n');
-	return (0);
 }
