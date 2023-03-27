bd1e2747cc076e00157c34319c5b9f246e5711ea
Mcrymbo <croco2023@gmail.com>
2023-03-27 12:03:23 -0700

    print string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 	int i, j;
 
 	i = 0;
-	while(s[i] != 0)
+	while (s[i] != 0)
 		i++;
-	for(j = i - 1; j >= 0; j--)
+	for (j = i - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
