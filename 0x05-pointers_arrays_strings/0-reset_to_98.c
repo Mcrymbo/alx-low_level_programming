d433e8ccb553a7e70754b2d74b8af361a035ad00
Mcrymbo <croco2023@gmail.com>
2023-03-27 09:51:36 -0700

    reset variables using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset variables using pointers
+ *
+ * @c: prints character
+ * Return: 1 for success and -1 otherwise
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
