ba3765ce1831ce9731f1bf2540c754aeec7abd0d
Mcrymbo <croco2023@gmail.com>
2023-04-03 21:09:49 +0000

    length of prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - get length of prefix substring
+ * @s: string
+ * @accept: second bytes
+ * Return: number of string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j = 0, k = 0;
+	
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] != ' ')
+		{
+			for (; accept[j] != '\0'; j++)
+			{
+				if (s[j] == accept[j])
+				{
+					k++;
+				}
+			}
+		}
+		else
+			return (k);
+	}
+	return (k);
+}
