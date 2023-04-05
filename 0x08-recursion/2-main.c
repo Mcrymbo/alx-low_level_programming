908c395d85b87d5f058480a9f0badbdcfd9f5ac6
Mcrymbo <croco2023@gmail.com>
2023-04-05 10:04:16 +0000

    length of string

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
