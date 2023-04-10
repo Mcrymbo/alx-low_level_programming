b5b65f91af8fc791c0a37ea434f5a34c81a181fe
Mcrymbo <croco2023@gmail.com>
2023-04-10 05:13:36 +0000

    prints the number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - number of arguments
+ * @argc: string counts
+ * @argv: store string variables
+ * Return: number of strings
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
