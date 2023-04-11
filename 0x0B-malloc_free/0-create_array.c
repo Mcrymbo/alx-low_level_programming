1baf21de2a9b49fd3bc72b1c1053b106d9f72498
Mcrymbo <croco2023@gmail.com>
2023-04-11 18:42:14 +0000

    creating array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	i = 0;
 	array = malloc(sizeof(c) * size);
 	if (size == 0)
-		return ('\0');
+		return (0);
 	for (i = 0; i < size; i++)
 	{
 		*(array + i) = c;
