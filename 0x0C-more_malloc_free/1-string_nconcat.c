639039fc31cbae9876ad8a7afaaeeca2d54eda87
Mcrymbo <croco2023@gmail.com>
2023-04-13 11:00:07 +0000

    concates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,10 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
 	}
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 	s = malloc(strlen(s1) + n + 1);
 	if (s == NULL)
 		return (NULL);
