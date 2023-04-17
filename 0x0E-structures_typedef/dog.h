6e2ed51349dee806145152388b397f7ea77c24da
Mcrymbo <croco2023@gmail.com>
2023-04-17 08:34:28 +0000

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - contains information on a dog
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ *
+ * Description: contains information on a dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
