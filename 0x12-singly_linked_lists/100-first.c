30616cd2d36ecffc68b895b9b3daccba0a54553f
Mcrymbo <croco2023@gmail.com>
2023-04-27 19:34:10 +0000

    correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
-#include "lists.h"
+#include "stdio.h"
 
 /**
  * before_main - executed function before main
  * Return: no return
  */
-void __attribute__((constructor)) before_main(void)
+void __attribute__((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
 }
