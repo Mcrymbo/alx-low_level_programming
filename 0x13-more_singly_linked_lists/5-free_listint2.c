6ee0d09d03c7dfeee608230aa532e488ff485d75
Mcrymbo <croco2023@gmail.com>
2023-05-01 18:55:43 +0000

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ * @head: first node of the list
+ * Return: no return
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *temp1;
+	
+	if (head)
+	{
+		temp1 = *head;
+		while (!(temp = temp1))
+		{
+			temp1 = temp1->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
