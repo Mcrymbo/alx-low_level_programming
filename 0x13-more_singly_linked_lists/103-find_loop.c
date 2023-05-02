feb4b5d8a5f8b03a32966db7ec45869c68956412
Mcrymbo <croco2023@gmail.com>
2023-05-02 12:02:28 +0000

    find loop in kinked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find loop in a linked list
+ * @head: first node
+ * Return: address of node where loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *temp, *curr;
+
+	temp = head;
+	curr = head;
+	while (head && temp && temp->next)
+	{
+		head = head->next;
+		temp = temp->next->next;
+		if (head == temp)
+		{
+			head = curr;
+			curr = temp;
+			while(1)
+			{
+				temp = curr;
+				while (temp->next != head && temp->next != curr)
+					temp = temp->next;
+				if (temp->next == head)
+					break;
+				head = head->next;
+			}
+			return (temp->next);
+		}
+	}
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -46,5 +46,6 @@ typedef struct listint_v
 void free_lst(listint_v **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
