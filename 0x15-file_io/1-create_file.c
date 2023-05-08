1ba1cbdfc0627f5c4090319701885a37e59598de
Mcrymbo <croco2023@gmail.com>
2023-05-08 17:53:12 +0000

    handling null for text-content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,9 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1 || !text_content)
+	if (!text_content)
+		text_content = "";
+	if (fd == -1)
 		return (-1);
 	for (i = 0; text_content[i]; i++)
 		;
