b45448a64b8491548de943425f7bbb0e45b5dca7
Mcrymbo <croco2023@gmail.com>
2023-07-06 15:41:00 +0000

    binari to unsigned int

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints character
+ * @c: character to print
+ * Return: 1 for success of -1 otherwise
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
