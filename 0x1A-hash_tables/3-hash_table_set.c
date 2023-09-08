2f823ddc769833631a2c5756179a54c28fe9dd5a
Mcrymbo <croco2023@gmail.com>
2023-09-08 09:50:43 +0000

    correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * add_hash_node - adds not at an the begining of a hash index
+ * add_hsh_nd - adds not at an the begining of a hash index
  * @ht: head of a hashed table
  * @key: key of the hash table
  * @value: value to store
  * Return: ht
  */
-hash_node_t *add_hash_node(hash_node_t **ht, const char *key, const char *value)
+hash_node_t *add_hsh_nd(hash_node_t **ht, const char *key, const char *value)
 {
 	hash_node_t *temp = *ht;
 
@@ -45,7 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || *key == '\0')
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	if (add_hash_node(&(ht->array[index]), key, value) == NULL)
+	if (add_hsh_nd(&(ht->array[index]), key, value) == NULL)
 		return (0);
 	return (1);
 }
