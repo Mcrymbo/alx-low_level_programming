6ef216926671abf9da7052f69ea7b513e23a9d4d
Mcrymbo <croco2023@gmail.com>
2023-09-08 07:23:32 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 	new->array = malloc(sizeof(hash_node_t) * size);
 	if (new->array == NULL)
 	{
