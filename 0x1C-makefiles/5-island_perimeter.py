a306f4b1f8ac557f3682d5c5fde14dace5618e9e
Mcrymbo <croco2023@gmail.com>
2023-10-25 22:12:42 +0300

    all files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+module that returns the perimeter of an island
+"""
+
+
+def island_perimeter(grid):
+    """ function that calculates perimeter of an island """
+    perimeter = 0;
+    rows = len(grid)
+    if len(grid) != 0:
+        columns = len(grid[0])
+
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == 1:
+                if (i - 1) == -1 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if (i + 1) == rows or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if (j - 1) == -1 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if (j + 1) == columns or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,2 @@
+# 0x1C. C - Makefiles
+This is a readme file for c makefile
